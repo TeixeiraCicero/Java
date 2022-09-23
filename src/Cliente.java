2d9f7ef51bbddbb3bea2ea8136b20bd5282ec055
TeixeiraCicero <cicero.rover18@gmail.com>
2022-09-23 09:56:49 -0300

    cliente e maincliente

---
+++ src/Cliente.java
@@ -0,0 +1,73 @@
+
+public class Cliente {
+	private int id;
+	private String nomeCompleto;
+	private int cpf;
+	private String numeroCelular;
+	private String rua;
+	private String numeroRua;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+	public Cliente(int id, int cpf, String numeroCelular, String nomeCompeto, String rua, String numeroRua,
+			String complemento, String bairro, String cidade, String estado) {
+		this.id = id;
+		this.cpf = cpf;
+		this.nomeCompleto = nomeCompeto;
+		this.numeroCelular = numeroCelular;
+		this.rua = rua;
+		this.numeroRua = numeroRua;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		System.out.println("Cliente criado com sucesso.");
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public String getNumeroCelular() {
+		return numeroCelular;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public String getNumeroRua() {
+		return numeroRua;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
---
+++ src/MainCliente.java
@@ -0,0 +1,29 @@
+
+public class MainCliente {
+
+	public static void main(String[] args) {
+		Cliente cli1 = new Cliente(1, 123456789, "(45)988521548", "Ana", "AV Ayrtor Neves", "18845", "Sobrado 5",
+				"Centro", "Foz do Iguaçu", "Parana");
+		Cliente cli2 = new Cliente(2, 987654321, "(43)981548748", "Eli", "AV Mundo Alegre", "145", "Casa", "Centro",
+				"Cascavel", "Parana");
+		Cliente cli3 = new Cliente(3, 147852369, "(41)954831548", "Gabi", "AV Terra roxa", "1815", "Apartamento 5",
+				"Mundo novo", "Curitiba", "Parana");
+
+		System.out.println("o ID e " + cli1.getId() + ", o nome do Cliente e " + cli1.getNomeCompleto() + ", seu CPF e "
+				+ cli1.getCpf() + ", seu numero de Celular e " + cli1.getNumeroCelular() + ", Mora no Estado "
+				+ cli1.getEstado() + ", na Cidade " + cli1.getCidade() + ", no Bairro " + cli1.getBairro() + ", na Rua "
+				+ cli1.getRua() + ", no Numero " + cli1.getNumeroRua() + ", e o complemento e "
+				+ cli1.getComplemento());
+		System.out.println("o ID e " + cli2.getId() + ", o nome do Cliente e " + cli2.getNomeCompleto() + ", seu CPF e "
+				+ cli2.getCpf() + ", seu numero de Celular e " + cli2.getNumeroCelular() + ", Mora no Estado "
+				+ cli2.getEstado() + ", na Cidade " + cli2.getCidade() + ", no Bairro " + cli2.getBairro() + ", na Rua "
+				+ cli2.getRua() + ", no Numero " + cli2.getNumeroRua() + ", e o complemento e "
+				+ cli2.getComplemento());
+		System.out.println("o ID e " + cli3.getId() + ", o nome do Cliente e " + cli3.getNomeCompleto() + ", seu CPF e "
+				+ cli3.getCpf() + ", seu numero de Celular e " + cli3.getNumeroCelular() + ", Mora no Estado "
+				+ cli3.getEstado() + ", na Cidade " + cli3.getCidade() + ", no Bairro " + cli3.getBairro() + ", na Rua "
+				+ cli3.getRua() + ", no Numero " + cli3.getNumeroRua() + ", e o complemento e "
+				+ cli3.getComplemento());
+
+	}
+}
