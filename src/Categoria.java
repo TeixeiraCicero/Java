09bb88db83c26c68ec4d80bc1c1f1eab21c7cfe8
TeixeiraCicero <cicero.rover18@gmail.com>
2022-09-28 14:36:04 -0300

    Categoria e MainCategoria

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,15 +1,35 @@
-
 public class Categoria {
 
 	private int id;
 	private static int idsozinho = 1;
 	private String nome;
 	private StatusCategoria status;
-	
-	public Categoria() {
-		this.id = idsozinho++;
+
+	public Categoria(int id, String nome) {
+		if (id <= 0) {
+			throw new IllegalArgumentException("ID Invalido");
+		} else
+			this.id = id;
+		if (nome.length() > 3)
+			this.nome = nome;
+		else {
+			throw new IllegalArgumentException("Nome Invalido");
+		}
+		this.status = StatusCategoria.ATIVA;
 	}
-	
+
+	public Categoria(int id, String nome, StatusCategoria status) {
+		if (id <= 0) {
+			throw new IllegalArgumentException("ID Invalido");
+		} else
+			this.id = id;
+		if (nome.length() > 3)
+			this.nome = nome;
+		else {
+			throw new IllegalArgumentException("Nome Invalido");
+		}
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -32,5 +52,10 @@ public class Categoria {
 
 	public void setStatus(StatusCategoria status) {
 		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria " + getNome() + " ( " + getId() + " - " + getStatus() + " ). ";
 	}
 }
--- src/MainCategoria.java
+++ src/MainCategoria.java
@@ -1,27 +1,13 @@
 
 public class MainCategoria {
 
-	public static void main(String[] args) {
-
-		Categoria cat1 = new Categoria();
-		Categoria cat2 = new Categoria();
-		Categoria cat3 = new Categoria();
-
-		//cat1.setId(1);
-		cat1.setNome("INFORMATICA");
-		cat1.setStatus(StatusCategoria.ATIVA);
-
-		//cat2.setId(2);
-		cat2.setNome("MOVEIS");
-		cat2.setStatus(StatusCategoria.INATIVA);
+	public static void main(String[] args) {//CONSTRUTOR id, nome,(OPCional) statusCategoria
+//		Categoria cat1 = new Categoria(0, "INFORMATICA");// ID invalido
+//		Categoria cat1 = new Categoria(2, "nom",StatusCategoria.INATIVA);//NOME invalido
+		Categoria cat1 = new Categoria(3, "Livro",StatusCategoria.ATIVA);//funcionando
 		
-		//cat3.setId(3);
-		cat3.setNome("LIVROS");
-		cat3.setStatus(StatusCategoria.ATIVA);
+		System.out.println(cat1.toString());
 		
-		System.out.println("Categoria " + cat1.getNome() + " ( " + cat1.getId() + " - " + cat1.getStatus()+" ). ");
-		System.out.println("Categoria " + cat2.getNome() + " ( " + cat2.getId() + " - " + cat2.getStatus()+" ). ");
-		System.out.println("Categoria " + cat3.getNome() + " ( " + cat3.getId() + " - " + cat3.getStatus()+" ). ");
-	}
 
+}
 }
