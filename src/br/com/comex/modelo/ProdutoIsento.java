e415e34ae013ce0fcd2ddf576ef28c9a882549dd
TeixeiraCicero <cicero.rover18@gmail.com>
2022-09-29 10:25:02 -0300

    ProdutoIsento

--- src/br/com/comex/modelo/ProdutoIsento.java
+++ src/br/com/comex/modelo/ProdutoIsento.java
@@ -1,8 +1,7 @@
 package br.com.comex.modelo;
 
 public class ProdutoIsento extends Produto {
-	
-	
+
 	public ProdutoIsento(int id, String nomeProduto, double precoUnitario, int qtdEstoque, String categoria) {
 		super(id, nomeProduto, precoUnitario, qtdEstoque, categoria);
 	}
