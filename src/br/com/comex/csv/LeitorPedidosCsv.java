f022cccd100411b25b5fac997ea57bb241ae9558
TeixeiraCicero <cicero.rover18@gmail.com>
2022-10-07 17:49:33 -0300

    PedidoCsv e LeitorPedidosCsv

---
+++ src/br/com/comex/csv/LeitorPedidosCsv.java
@@ -0,0 +1,35 @@
+package br.com.comex.csv;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LeitorPedidosCsv {
+	public ArrayList<PedidoCsv> leCsv() throws FileNotFoundException {
+		ArrayList<PedidoCsv> pedidos = new ArrayList<PedidoCsv>();
+
+		InputStream input = new FileInputStream("pedidos.csv");
+		Scanner scan = new Scanner(input);
+
+		scan.nextLine();
+
+		while (scan.hasNextLine()) {
+			String linha = scan.nextLine();
+			String[] valores = linha.split(",");
+			// CATEGORIA,PRODUTO,PRECO,QUANTIDADE,DATA,CLIENTE
+			String categoria = valores[0];
+			String produto = valores[1];
+			String preco = valores[2];
+			String quantidade = valores[3];
+			String data = valores[4];
+			String cliente = valores[5];
+
+			PedidoCsv pedido = new PedidoCsv(categoria, produto, preco, quantidade, data, cliente);
+
+			pedidos.add(pedido);
+		}
+		return pedidos;
+	}
+}
