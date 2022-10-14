38ce8dfad3db546f007f33056c7c15ace5559b5c
TeixeiraCicero <cicero.rover18@gmail.com>
2022-10-14 16:05:11 -0300

    testa conexao

---
+++ src/br/com/comex/main/MainTestaConexao.java
@@ -0,0 +1,19 @@
+package br.com.comex.main;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.comex.modelo.ConnectionFactory;
+
+public class MainTestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory Conexao = new ConnectionFactory();
+		Connection conect = Conexao.Conect();
+		
+		System.out.println("Conexão efetuada com sucesso");
+		conect.close();
+		
+	}
+
+}
