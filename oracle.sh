ad7b8fc8dfb1d17b5e359623f3e019d5fd50d132
TeixeiraCicero <cicero.rover18@gmail.com>
2022-10-28 15:31:28 -0300

    oracle volume

--- oracle.sh
+++ oracle.sh
@@ -1,2 +1,3 @@
-docker run -d --name oracleXe -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker volume create oracle-volume
+docker run -d -v oracle-volume:/opt/oracle/oradata --name oracleXe -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
 docker container logs oracleXe
